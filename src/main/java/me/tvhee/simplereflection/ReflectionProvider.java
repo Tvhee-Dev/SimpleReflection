8c7b1212de49416a61ce159650037d6de04d7657
tvhee-dev <tvhee100@gmail.com>
2021-12-17 21:02:11 +0100

    Fixed class issues

--- src/main/java/me/tvhee/simplereflection/ReflectionProvider.java
+++ src/main/java/me/tvhee/simplereflection/ReflectionProvider.java
@@ -23,25 +23,14 @@ public class ReflectionProvider
 		return new SimpleReflection(instance);
 	}
 
-	public static boolean hasMinecraftClass(String before1_17, String after1_17)
-	{
-		try
-		{
-			return getMinecraftClass(before1_17, after1_17) != null;
-		}
-		catch(Exception e)
-		{
-			return false;
-		}
-	}
-
 	public static boolean hasClass(String name)
 	{
 		try
 		{
-			return getClass(name) != null;
+			getClass0(name);
+			return true;
 		}
-		catch(Exception e)
+		catch(ClassNotFoundException e)
 		{
 			return false;
 		}
@@ -60,22 +49,74 @@ public class ReflectionProvider
 		}
 	}
 
-	public static Class<?> getCraftBukkitClass(String name)
+	public static Class<?> getClass0(String name) throws ClassNotFoundException
 	{
-		return getClass(MinecraftVersion.getVersion().replace(name));
+		return Class.forName(name);
+	}
+
+	public static boolean hasMinecraftClass(String before1_17, String after1_17)
+	{
+		try
+		{
+			getMinecraftClass0(before1_17, after1_17);
+			return true;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
 	}
 
 	public static Class<?> getMinecraftClass(String before1_17, String after1_17)
 	{
-		if(MinecraftVersion.getVersion().atLeast(MinecraftVersion.v1_17_R1))
-			return getClass(MinecraftVersion.getVersion().replace(after1_17));
-		else
-			return getClass(MinecraftVersion.getVersion().replace(before1_17));
+		try
+		{
+			return getMinecraftClass0(before1_17, after1_17);
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
 	}
 
-	public static Class<? extends Enum<?>> getEnumClass(String name)
+	private static Class<?> getMinecraftClass0(String before1_17, String after1_17) throws ClassNotFoundException
 	{
-		return (Class<? extends Enum<?>>) getClass(name);
+		if(MinecraftVersion.getVersion().atLeast(MinecraftVersion.v1_17_R1))
+			return getClass0(MinecraftVersion.getVersion().replace(after1_17));
+		else
+			return getClass0(MinecraftVersion.getVersion().replace(before1_17));
+	}
+
+	public static boolean hasCraftBukkitClass(String name)
+	{
+		try
+		{
+			getCraftBukkitClass0(name);
+			return true;
+		}
+		catch(ClassNotFoundException e)
+		{
+			return false;
+		}
+	}
+
+	public static Class<?> getCraftBukkitClass(String name)
+	{
+		try
+		{
+			return getCraftBukkitClass0(name);
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static Class<?> getCraftBukkitClass0(String name) throws ClassNotFoundException
+	{
+		return getClass0(MinecraftVersion.getVersion().replace(name));
 	}
 
 	public static boolean parametersEquals(Class<?>[] parameters, Class<?>[] requestedParameters)
